4e98883a01458d89a8cfd4b764e3aafdb09a64a6
Gabriel Bohrer Mattje <gabrielmattje70@gmail.com>
2026-03-04 21:58:21 -0300

    Criando hello World

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args){
+           escreverHelloWorld();
+    }
+
+    public static void escreverHelloWorld(){
+        System.out.println("Hello World!");
+    }
+}
