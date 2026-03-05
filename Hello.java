98678dacf2f3bd290cb4b9781b4285d2edf261ea
Gabriel Bohrer Mattje <gabrielmattje70@gmail.com>
2026-03-04 22:03:18 -0300

    Escrevendo Ciao Mondo

--- Hello.java
+++ Hello.java
@@ -1,9 +1,16 @@
 public class Hello {
     public static void main(String[] args){
            escreverHelloWorld();
+           Italiaano.escreverCiaoMondo();
     }
 
     public static void escreverHelloWorld(){
         System.out.println("Hello World!");
+    }
+}
+
+class Italiaano{
+    public static void escreverCiaoMondo(){
+        System.out.println("Ciao Mondo!");
     }
 }
